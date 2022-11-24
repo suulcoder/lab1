44be42cecf15b95c5bff76245ddf40d3864c0609
Jose Block <blockmann3@gmail.com>
2022-11-23 18:17:34 -0600

    less than

--- Tests/3.cl
+++ Tests/3.cl
@@ -2,11 +2,13 @@ class Main {
    
    a : Int  <- 60;
    b : Int  <- 15;
-   c : Int;
+   c : Bool;
+   d : Int <- 1;
 
    main() : Int {
       {
-         c <- a / b;
+         c <- b < a;
+         if c then d <- 1000 else d <- 0 fi;
          0;
       }
    };
--- intermediate_code.py
+++ intermediate_code.py
@@ -240,7 +240,19 @@ def get_per_line(lines):
             last = asignations.get(words[4])
             code.append("div " + temporal + ", " + first + ", " + last)
             asignations[words[0]] = temporal
-            
+        elif "<" in line and "<<" not in line:
+            temporal = "$t" + get_temporal()
+            first = asignations.get(words[2])
+            last = asignations.get(words[4])
+            code.append("slt " + temporal + ", " + first + ", " + last)
+            asignations[words[0]] = temporal
+        # elif "<=" in line:
+        #     temporal = "$t" + get_temporal()
+        #     first = asignations.get(words[2])
+        #     last = asignations.get(words[4])
+        #     code.append("slt " + temporal + ", " + first + ", " + last)
+        #     asignations[words[0]] = temporal
+
 
             #Acá hay que poner todas las operaciones aritmeticas y lógicas
             # la resta, la multi, la division, los mayor que, los mayor o igual
